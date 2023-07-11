bc0e857e83afa76a195a206bb29821ebc8acb821
diya <msdiyamishra@gmail.com>
2023-07-11 18:20:23 -0400

    Initial Commit

---
+++ esteeLauder/esteeLauderApp.swift
@@ -0,0 +1,17 @@
+//
+//  esteeLauderApp.swift
+//  esteeLauder
+//
+//  Created by Saurabh Mishra on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct esteeLauderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
